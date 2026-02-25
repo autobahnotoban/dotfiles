7299127350cc4848f4547ba1a0acee5a977f0248
atahan <arabaciatahan@gmail.com>
2026-02-25 10:04:12 +0300

    inital commit

---
+++ nvim/init.lua
@@ -0,0 +1,219 @@
+-- 1. Basics
+vim.g.mapleader = " "
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.termguicolors = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+
+-- 2. Bootstrap Lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- 3. Plugins
+require("lazy").setup({
+	-- UI & Theme
+	{
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("gruvbox")
+		end,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("lualine").setup()({
+				options = {
+					theme = "catppuccin",
+					component_seperators = { left = "□", right = "□" },
+					section_seperators = { left = "□", right = "□" },
+					globalstatus = true,
+				},
+
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = { "branch", "diff", "diagnostics" },
+					lualine_c = { "filename" },
+					lualine_x = { "encoding", "fileformat", "filetype" },
+					lualine_y = { "progress" },
+					lualine_z = { "location" },
+				},
+			})
+		end,
+	},
+
+	{
+		"nvim-tree/nvim-tree.lua",
+		version = "*",
+		lazy = false,
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("nvim-tree").setup({})
+			vim.keymap.set("n", "<C-n>", "NvimTreeToggle<CR>", { silent = true })
+		end,
+	},
+
+	-- Which-Key (The shortcut helper)
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		config = function()
+			require("which-key").setup()
+		end,
+	},
+
+	-- Gitsigns (Git status in the sidebar)
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+		end,
+	},
+
+	-- Telescope (The Fuzzy Finder)
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Search Text" })
+		end,
+	},
+
+	-- Conform (Auto-formatting for C, Python, and Go)
+	{
+		"stevearc/conform.nvim",
+		config = function()
+			require("conform").setup({
+				formatters_by_ft = {
+					lua = { "stylua" },
+					python = { "black" },
+					c = { "clang-format" },
+					go = { "goimports", "gofmt" }, -- Auto-adds imports on save!
+				},
+				format_on_save = { timeout_ms = 500, lsp_fallback = true },
+			})
+		end,
+	},
+
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "mocha",
+				transparent_background = false,
+				term_colors = true,
+				integrations = {
+					cmp = true,
+					gitsigns = true,
+					nvimtree = true,
+					treesitter = true,
+					notify = true,
+					mini = {
+						enabled = true,
+						indentscope_color = "",
+					},
+					lualine = true,
+				},
+			})
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
+
+	-- Treesitter (Advanced Highlighting)
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter").setup({
+				ensure_installed = { "lua", "python", "c", "gomod", "gowork" },
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = false,
+				},
+				indent = { enable = true },
+			})
+		end,
+	},
+
+	-- LSP & Autocompletion
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"hrsh7th/nvim-cmp",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+		},
+		config = function()
+			require("mason").setup()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "pyright", "clangd", "lua_ls", "gopls" },
+				handlers = {
+					function(server_name)
+						require("lspconfig")[server_name].setup({
+							capabilities = require("cmp_nvim_lsp").default_capabilities(),
+						})
+					end,
+				},
+			})
+
+			local cmp = require("cmp")
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<Tab>"] = cmp.mapping.select_next_item(),
+					["<C-Space>"] = cmp.mapping.complete(),
+				}),
+				sources = cmp.config.sources(
+					{ { name = "nvim_lsp" }, { name = "luasnip" } },
+					{ { name = "buffer" }, { name = "path" } }
+				),
+			})
+		end,
+	},
+})
+
+-- 4. Keymaps (Your Favorites + New Power Moves)
+local map = vim.keymap.set
+
+-- Your Core List
+map("n", "<leader>j", ":NvimTreeToggle<cr>", { desc = "Toggle Explorer" })
+map("n", "<leader>tn", ":tabnext<cr>", { desc = "Next Tab" })
+map("n", "<leader>tp", ":tabprevious<cr>", { desc = "Previous Tab" })
+map("n", "<leader>tc", ":tabnew<cr>", { desc = "Create New Tab" })
+map("n", "<leader>tx", ":tabclose<cr>", { desc = "Close Current Tab" })
+-- Tab is handled automatically inside the 'cmp.setup' above for autocompletion.
+
+-- New Essential Engineering Shortcuts
+map("n", "<leader>ff", ":Telescope find_files<cr>", { desc = "Find Files" })
+map("n", "<leader>fg", ":Telescope live_grep<cr>", { desc = "Search inside Files" })
+map("n", "gd", vim.lsp.buf.definition, { desc = "Go to Definition" })
+map("n", "K", vim.lsp.buf.hover, { desc = "Hover Documentation" })
+map("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename Variable Everywhere" })
